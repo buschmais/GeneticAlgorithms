a3d1991bda2943ba8e11061c3a368995dff5bab9
StephanPirnbaum <Stephan.Pirnbaum@googlemail.com>
2017-11-27 20:55:14 +0100

    Added jenetics implementation of the infinite monkey theorem

---
+++ infinite-monkeys/jenetics/src/main/java/com/buschmais/javaspektrum/geneticalgorithms/infinitemonkeys/jenetics/JeneticsExample.java
@@ -0,0 +1,66 @@
+package com.buschmais.javaspektrum.geneticalgorithms.infinitemonkeys.jenetics;
+
+import org.jenetics.*;
+import org.jenetics.engine.Engine;
+import org.jenetics.engine.limit;
+import org.jenetics.util.CharSeq;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Infinite monkey theorem implementation utilizing the Jenetics framework
+ *
+ * @author Stephan Pirnbaum
+ */
+public class JeneticsExample {
+
+    private static final String target = "to be or not to be";
+
+    public static void main(String[] args) {
+        final StringBuilder allels = new StringBuilder(" ");
+        IntStream.rangeClosed(97, 122).forEach(i -> allels.append((char) i));
+
+        /*
+         * Specify the genetic structure: number of chromosomes, allowed allels, and length
+         */
+        Genotype<CharacterGene> genotype = Genotype.of(
+                new CharacterChromosome(new CharSeq(allels), 18)
+        );
+        /*
+         * Create the evolution engine
+         */
+        Engine<CharacterGene, Double> engine = Engine
+                .builder(JeneticsExample::fitness, genotype)
+                .fitnessFunction(JeneticsExample::fitness)
+                .populationSize(100)
+                .alterers(new SinglePointCrossover<>(1), new Mutator<>(0.01))
+                .selector(new RouletteWheelSelector<>())
+                .build();
+        /*
+         * Evolve the population until the solution is found. Take notice of double precision.
+         */
+        engine.stream()
+                .limit(limit.byFitnessThreshold(0.9999999d))
+                .peek(g -> System.out.println("Generation: " + g.getGeneration() + " Best: " + g.getBestPhenotype()))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Computes the fitness of a solution based on the number of correct characters
+     *
+     * @param individual The individual to compute the fitness for
+     *
+     * @return The fitness
+     */
+    private static Double fitness(final Genotype<CharacterGene> individual) {
+        int fitness = 0;
+        Chromosome<CharacterGene> chromosome = individual.getChromosome(0);
+        for (int i = 0; i < individual.getChromosome(0).length(); i++) {
+            if (chromosome.getGene(i).getAllele() == target.charAt(i)) {
+                fitness++;
+            }
+        }
+        return (double) fitness / target.length();
+    }
+}
